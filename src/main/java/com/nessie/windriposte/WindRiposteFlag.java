fac803e3f603bdd8967de8294c078c79551a8e08
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-13 17:36:41 -0800

    Delete src/main directory

--- src/main/java/com/nessie/windriposte/WindRiposteFlag.java
+++
@@ -1,7 +0,0 @@
-package com.nessie.windriposte;
-
-public interface WindRiposteFlag {
-    void windriposte$markShieldDisabled(long tick);
-    long windriposte$getShieldDisabledTick();
-    void windriposte$clearShieldDisabled();
-}
