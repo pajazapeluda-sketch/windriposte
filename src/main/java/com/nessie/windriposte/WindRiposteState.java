2639063a46005fc8e621ca3d89ffde302af7c590
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-13 16:36:29 -0800

    Update WindRiposteState.java

--- src/main/java/com/nessie/windriposte/WindRiposteState.java
+++ src/main/java/com/nessie/windriposte/WindRiposteState.java
@@ -3,16 +3,11 @@ package com.nessie.windriposte;
 import net.minecraft.entity.LivingEntity;
 
 public interface WindRiposteState {
-    void windriposte$setLastAttacker(LivingEntity attacker);
     LivingEntity windriposte$getLastAttacker();
+    void windriposte$setLastAttacker(LivingEntity attacker);
+
+    int windriposte$getLastLevel();
+    void windriposte$setLastLevel(int level);
+
     void windriposte$clearLastAttacker();
-}
-private int windriposte$lastLevel = 0;
-
-public void windriposte$setLastLevel(int level) {
-    this.windriposte$lastLevel = level;
-}
-
-public int windriposte$getLastLevel() {
-    return this.windriposte$lastLevel;
 }
