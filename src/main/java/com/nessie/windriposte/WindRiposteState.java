2af9b9f92075cce91f545f2b5ddcd3b06431a144
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-13 16:23:35 -0800

    Update WindRiposteState.java

--- src/main/java/com/nessie/windriposte/WindRiposteState.java
+++ src/main/java/com/nessie/windriposte/WindRiposteState.java
@@ -7,3 +7,12 @@ public interface WindRiposteState {
     LivingEntity windriposte$getLastAttacker();
     void windriposte$clearLastAttacker();
 }
+private int windriposte$lastLevel = 0;
+
+public void windriposte$setLastLevel(int level) {
+    this.windriposte$lastLevel = level;
+}
+
+public int windriposte$getLastLevel() {
+    return this.windriposte$lastLevel;
+}
